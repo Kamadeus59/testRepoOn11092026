819d8ceadb8fd14fc97f2681c074bba10ad5c107
Kamadeus59 <kamadeus59@gmail.com>
2026-09-11 22:05:50 +0200

    adding render function

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ pg.init()
 
 screen = pg.display.set_mode((500,500))
 
+def Render(screen):
+    pg.draw.rect(screen, (255,255,255), (100,100,100,100))
+
+
 running = True
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
     screen.fill((0,0,0))
-    pg.draw.rect(screen, (255,255,255), (100,100,100,100))
+    Render(screen)
+    
     pg.display.flip()
 pg.quit()
